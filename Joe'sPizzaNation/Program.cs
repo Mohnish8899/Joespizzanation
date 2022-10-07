7bcfa195e7511b605c98a847fe8e056cc4692116
Mohnish8899 <mohnishbaviskar99@gmail.com>
2022-10-07 14:51:06 +0530

    Add project files.

---
+++ Joe'sPizzaNation/Program.cs
@@ -0,0 +1,23 @@
+var Constructor = WebApplication.CreateBuilder(args);
+
+Constructor.Services.AddRazorPages();
+
+var app = Constructor.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
